b9a42e7ffbf67d73d2310b91e62c1d54269d9721
joansalcedo1 <sebastiansalcedo7@gmail.com>
2025-07-21 21:12:02 -0500

    Haciendo conexion mongoDB

---
+++ api-Portafolio2/modules/proyectos.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const ProyectoSchema = new Schema({
+    nombre:{
+        type: String,
+        required: true,
+        maxLength:100
+    },
+    imagen:{
+        type:String,
+        required: true,
+        maxLength:100
+    },
+    repo:{
+        type:String,
+        required:true,
+        maxLength:100
+    },
+    tecnologias:{
+        type:[String]
+    },
+    descripcion:{
+        type:String,
+        required: true,
+        maxLength:500 
+    }
+
+
+})
--- api-portafolio/controllers/controllers_proyecto.js
+++
@@ -1,4 +0,0 @@
-exports.Hola = (req,res)=>{
-    console.log("Hola desde el controlador")
-    res.send("Hola desde el controlador")
-}
