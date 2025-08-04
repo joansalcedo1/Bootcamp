4d737b03bfd619e1c2eb889d5029a35663887651
joansalcedo1 <sebastiansalcedo7@gmail.com>
2025-08-04 10:35:08 -0500

    Biblioteca
    Biblioteca cada vez más completa

--- ejercicio_practicos_React/libreriaEjercicioReact/src/App.jsx
+++ ejercicio_practicos_React/libreriaEjercicioReact/src/App.jsx
@@ -1,13 +1,18 @@
 import NavBar from "./components/NavBar"
 import Libros from "./components/Libros"
-import OurServices from "./components/OurServices"
-
+import OurServices from "./components/Services"
+import Comprometidos from "./components/Comprometidos"
+import Parallax from "./components/Parallax"
 function App() {
   return (
     <>
       <NavBar />
+      
       <Libros />  
+      <Parallax/>
+      <Comprometidos/>
       <OurServices/>
+
     </>
   )
 }
--- ejercicio_practicos_React/libreriaEjercicioReact/src/components/Libros.jsx
+++ ejercicio_practicos_React/libreriaEjercicioReact/src/components/Libros.jsx
@@ -1,23 +1,28 @@
 import { useState, useEffect } from "react";
 //import librosData from "../data/DataLibros.json"
 import CardLibros from "./CardLibros";
-import { getAllBooks } from "../services/getBooks";
+import { getAllBooks, } from "../services/getBooks";
+
+
 function Libros() {
     const [libros, setLibros] = useState([])
     useEffect(() => {
+
         async function fetchData() {
             const data = await getAllBooks()
             console.log(data)
             setLibros(data[0])
         }
-        
-        fetchData()
+
     }, [])
     return (
         <>
             <section>
-                <h1 className="text-center">Mis libros<h4> ({libros.length})</h4></h1>
-                  <p className="m-3 text-center">En esta pagina web encontrarás informacion sobre todos los libros que se encuentran en nuestra base de datos</p>
+
+                <h2 className="text-center">Mis libros 
+                    <p className="fs-5"> ({libros.length})</p>
+                </h2>
+                <p className="m-3 text-center">En esta pagina web encontrarás informacion sobre todos los libros que se encuentran en nuestra base de datos</p>
                 <div className="bg-primary row justify-content-center">
                     {libros.map((libro, i) => (
                         <CardLibros
@@ -25,7 +30,7 @@ function Libros() {
                             libro={libro} />
                     ))}
                 </div>
-                
+
             </section>
         </>
     )
